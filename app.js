eb4cbf7812dc1733fd8a52fa8011438271ad3817
Maria <maria.gutierrez>
2020-06-10 17:31:32 +0100

    first commit opening repository

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const width = 10;
+    const scoreDisplay = document.getElementById('#score');
+    const startBtn = document.getElementById('#start-button');
+
+console.log(squares);
+
+
+})
